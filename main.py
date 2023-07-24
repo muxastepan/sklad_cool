78c73b3ea7292bde14379f60b5f8f325f11bcb86
muxastepan <58739030+muxastepan@users.noreply.github.com>
2023-07-24 14:33:26 +0400

    ba

--- main.py
+++ main.py
@@ -1,29 +1,55 @@
-import re
 import tkinter as tk
-from abc import ABC
 from tkinter import ttk
 from typing import Union
-
+import re
 from sql_adapter import Adapter, ProductsTable, EmployeesTable
 import datetime
 
 
-class DataGridView:
-    def __init__(self, column_names: Union[list, tuple]):
-        self.name_labels, self.value_entries = self._build(column_names)
+class DataGridView(tk.Frame):
+    def __init__(self, column_names: Union[list, tuple], data):
+        super().__init__()
 
-    def _build(self, column_names):
-        name_labels = []
-        value_entries = []
-        for name in column_names:
-            name_labels.append(tk.Label(text=name))
-            value_entries.append(tk.Entry())
-        return name_labels, value_entries
+        self.column_names = column_names
+        self.data = data
+        self.scroll_bar = tk.Scrollbar(self)
+        self.scroll_bar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.table = ttk.Treeview(self)
+        self.scroll_bar.configure(command=self.table.yview)
+        self._build_table()
+        self.table.pack()
 
-    def show(self):
-        for i in range(len(self.name_labels)):
-            self.name_labels[i].grid(row=1, column=i)
-            self.value_entries[i].grid(row=2, column=i)
+    def sort_data(self, col, reverse):
+        data = [(self.table.set(child, col), child) for child in self.table.get_children('')]
+        data_vals = [x[0] for x in data]
+
+        if all(re.match('\d{4}-\d{2}-\d{2}', i) for i in data_vals):
+            data.sort(reverse=reverse, key=lambda x: datetime.datetime.strptime(x[0], '%Y-%m-%d'))
+        elif all(re.match('\d+', i) for i in data_vals):
+            data.sort(reverse=reverse, key=lambda x: int(x[0]))
+        else:
+            data.sort(reverse=reverse)
+        for index, (val, child) in enumerate(data):
+            self.table.move(child, '', index)
+
+        self.table.heading(col, command=lambda: self.sort_data(col, not reverse))
+
+    def _build_table(self):
+        self.table['columns'] = self.column_names
+        self.table.column('#0', width=0, stretch=tk.NO)
+        for i, heading in enumerate(self.column_names):
+            self.table.column(heading, anchor=tk.W, width=100)
+            self.table.heading(heading, text=heading, anchor=tk.W,
+                               command=lambda col=heading: self.sort_data(col, False))
+
+        self._row_count = 0
+        for row in self.data:
+            self.table.insert('', tk.END, values=row)
+            self._row_count += 1
+
+    def add_row(self, data):
+        self._row_count += 1
+        self.table.insert('', tk.END, values=data[0])
 
 
 class AutoCompletionCombobox(ttk.Combobox):
@@ -44,9 +70,43 @@ class AutoCompletionCombobox(ttk.Combobox):
             self['values'] = data
 
 
-class AddProductRecordDialogue(tk.Toplevel):
-    def __init__(self, products_table: ProductsTable, employees_table: EmployeesTable):
+class MainFrame(tk.Tk):
+    def __init__(self):
         super().__init__()
+
+        self.sql_adapter = Adapter('testdb')
+        self.employees_table = EmployeesTable(self.sql_adapter)
+        self.product_table = ProductsTable(self.sql_adapter, self.employees_table)
+        self.add_product_button = tk.Button(text='Добавить продукт', command=self.show_add_product_dialogue)
+        self.add_employee_button = tk.Button(text='Добавить сотрудника', command=self.show_add_employee_dialogue)
+        data_for_grid = self.product_table.select_all()
+        self.data_grid = DataGridView(('ID', 'Размер', 'Тип', 'Подтип', 'Цвет', 'Дата', 'Закл', 'Катка', 'Артикул'),
+                                      self.product_table.select_all())
+        # self.show_db_button = tk.Button(text='show', command=self.show_db)
+
+    def show_db(self):
+        self.data_grid.add_row(self.product_table.select_last_id())
+
+    def show_add_product_dialogue(self):
+        add_window = AddProductRecordDialogue(self, self.product_table, self.employees_table)
+        add_window.show()
+
+    def show_add_employee_dialogue(self):
+        add_window = AddEmployeeRecordDialogue(self, self.employees_table)
+        add_window.show()
+
+    def run(self):
+        self.add_product_button.grid(row=0, column=0, sticky=tk.EW)
+        self.add_employee_button.grid(row=0, column=1, sticky=tk.EW)
+        # self.show_db_button.grid(row=0, column=2, sticky=tk.EW)
+        self.data_grid.grid(row=1, column=0, columnspan=2, sticky=tk.NS)
+        self.mainloop()
+
+
+class AddProductRecordDialogue(tk.Toplevel):
+    def __init__(self, parent: MainFrame, products_table: ProductsTable, employees_table: EmployeesTable):
+        super().__init__()
+        self.parent = parent
         self.employees_table = employees_table
         self.products_table = products_table
         self.submit_button = tk.Button(self, text='Отправить', command=self.submit_close)
@@ -72,7 +132,7 @@ class AddProductRecordDialogue(tk.Toplevel):
         laid_by = self.employees_table.name_to_id(self.laid_by_entry.get())
         rolled_by = self.employees_table.name_to_id(self.rolled_by_entry.get())
         article = self.article_entry.get()
-        data_list = [
+        data = [
             size,
             p_type,
             p_subtype,
@@ -82,8 +142,9 @@ class AddProductRecordDialogue(tk.Toplevel):
             rolled_by,
             article
         ]
-        self.products_table.add(data_list)
-        self.products_table.update_unique_attrs(data_list[:4])
+        self.products_table.add(data)
+        self.products_table.update_unique_attrs(data[:4])
+        self.parent.data_grid.add_row(self.products_table.select_last_id())
         self.destroy()
 
     def show(self):
@@ -108,8 +169,9 @@ class AddProductRecordDialogue(tk.Toplevel):
 
 
 class AddEmployeeRecordDialogue(tk.Toplevel):
-    def __init__(self, employees_table: EmployeesTable):
+    def __init__(self, parent: MainFrame, employees_table: EmployeesTable):
         super().__init__()
+        self.parent = parent
         self.employees_table = employees_table
         self.submit_button = tk.Button(self, text='Отправить', command=self.submit_close)
         self.name_entry = AutoCompletionCombobox(self, values=employees_table.column_unique_attrs['employee_name'])
@@ -119,38 +181,13 @@ class AddEmployeeRecordDialogue(tk.Toplevel):
             self.name_entry.get()
         ]
         self.employees_table.add(data_list)
+        self.parent.data_grid.add_row(self.employees_table.select_last_id())
         self.destroy()
 
     def show(self):
         tk.Label(self, text='Имя').pack()
         self.name_entry.pack()
         self.submit_button.pack()
-
-
-class MainFrame(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        self.sql_adapter = Adapter('testdb')
-        self.product_table = ProductsTable(self.sql_adapter)
-        self.employees_table = EmployeesTable(self.sql_adapter)
-        self.add_product_button = tk.Button(text='Добавить продукт', command=self.show_add_product_dialogue)
-        self.add_employee_button = tk.Button(text='Добавить сотрудника', command=self.show_add_employee_dialogue)
-        self.add_window = None
-        self.data_grid = DataGridView(('Размер', 'Тип', 'Подтип', 'Цвет', 'Дата', 'Закл', 'Катка', 'Артикул'))
-
-    def show_add_product_dialogue(self):
-        self.add_window = AddProductRecordDialogue(self.product_table, self.employees_table)
-        self.add_window.show()
-
-    def show_add_employee_dialogue(self):
-        self.add_window = AddEmployeeRecordDialogue(self.employees_table)
-        self.add_window.show()
-
-    def run(self):
-        self.add_product_button.grid(row=0, column=0, sticky=tk.EW)
-        self.add_employee_button.grid(row=0, column=1, sticky=tk.EW)
-        self.data_grid.show()
-        self.mainloop()
 
 
 if __name__ == '__main__':
--- sql_adapter.py
+++ sql_adapter.py
@@ -17,21 +17,30 @@ class Adapter:
         self.cur.execute(f"INSERT INTO {table_name} {column_order_placeholder}VALUES {values_placeholder}", data_list)
         self.con.commit()
 
-    def select(self, table_name, columns: Union[list, tuple] = None, distinct=False, conditions: str = None):
-        if columns:
-            columns_placeholder = ','.join(columns)
-        else:
-            columns_placeholder = '*'
+    def select(self, table_name, columns: Union[list, tuple] = None, distinct=False, conditions: str = None,
+               order: Union[list, tuple] = None, limit=None):
+        query = "SELECT "
         if distinct:
-            distinct_placeholder = ' DISTINCT'
+            query += 'DISTINCT '
+        if columns:
+            query += ','.join(columns) + ' '
         else:
-            distinct_placeholder = ''
+            query += '* '
+
+        query += f'FROM {table_name}'
+
         if conditions:
-            conditions_placeholder = f' WHERE {conditions}'
-        else:
-            conditions_placeholder = f''
-        self.cur.execute(
-            f"SELECT{distinct_placeholder} {columns_placeholder} FROM {table_name}{conditions_placeholder}")
+            query += f' WHERE {conditions}'
+
+        if order:
+            query += f' ORDER BY {order[1]}'
+            if order[0]:
+                query += ' ASC'
+            else:
+                query += ' DESC'
+        if limit:
+            query += f' LIMIT {limit}'
+        self.cur.execute(query)
         data = self.cur.fetchall()
         self.con.commit()
         return data
@@ -44,10 +53,14 @@ class Adapter:
 
 
 class Table:
-    def __init__(self, adapter: Adapter, table_name: str, column_unique_attrs: Dict[str, list]):
+    def __init__(self, adapter: Adapter, table_name: str, column_unique_attrs: Dict[str, list], related_table=None):
+        self.related_table = related_table
         self.column_unique_attrs = column_unique_attrs
         self.table_name = table_name
         self.adapter = adapter
+
+    def select_last(self, column_name):
+        return self.adapter.select(self.table_name, order=(False, column_name), limit=1)
 
     def add(self, data_list):
         self.adapter.insert(self.table_name, data_list)
@@ -64,7 +77,7 @@ class Table:
     def select_where(self, columns: tuple, conditions: str):
         return self.adapter.select(self.table_name, columns, conditions=conditions)
 
-    def update_unique_attrs(self, values: Union[list,tuple]):
+    def update_unique_attrs(self, values: Union[list, tuple]):
         i = 0
         for attr, old_values in self.column_unique_attrs.items():
             if values[i] not in old_values:
@@ -72,8 +85,37 @@ class Table:
             i += 1
 
 
-class ProductsTable(Table):
+class EmployeesTable(Table):
     def __init__(self, adapter: Adapter):
+        super().__init__(adapter, 'employees',
+                         column_unique_attrs={
+                             'employee_name':
+                                 [item[0] for item in
+                                  adapter.select('employees', ('employee_name',), distinct=True)],
+                         }
+                         )
+
+    def select_last_id(self):
+        self.select_last('employee_id')
+
+    def add(self, data_list):
+        self.adapter.insert(self.table_name, data_list,
+                            column_order="(employee_name)")
+
+    def name_to_id(self, employee_name):
+        res_id = self.select_where(('employee_id',), f"employee_name = '{employee_name}'")
+        if not res_id:
+            self.add((employee_name,))
+            self.update_unique_attrs((employee_name,))
+            res_id = self.select_where(('employee_id',), f"employee_name = '{employee_name}'")
+        return res_id[0]
+
+    def id_to_name(self, employee_id):
+        return self.select_where(('employee_name',), f"employee_id = '{employee_id}'")[0]
+
+
+class ProductsTable(Table):
+    def __init__(self, adapter: Adapter, related_table: EmployeesTable):
         super().__init__(adapter, 'products',
                          column_unique_attrs={
                              'product_size':
@@ -88,32 +130,23 @@ class ProductsTable(Table):
                              'product_color':
                                  [item[0] for item in
                                   adapter.select('products', ('product_color',), distinct=True)]
-                         })
+                         },
+                         related_table=related_table)
 
     def add(self, data_list):
         self.adapter.insert(self.table_name, data_list,
                             column_order="(product_size,product_type, product_subtype,product_color,"
                                          "product_date_stored, laid_by,rolled_by,article)")
 
+    def select_last_id(self):
+        return self.select_last('product_id')
 
-class EmployeesTable(Table):
-    def __init__(self, adapter: Adapter):
-        super().__init__(adapter, 'employees',
-                         column_unique_attrs={
-                             'employee_name':
-                                 [item[0] for item in
-                                  adapter.select('employees', ('employee_name',), distinct=True)],
-                         }
-                         )
-
-    def add(self, data_list):
-        self.adapter.insert(self.table_name, data_list,
-                            column_order="(employee_name)")
-
-    def name_to_id(self, employee_name):
-        res_id = self.select_where(('employee_id',), f"employee_name = '{employee_name}'")
-        if not res_id:
-            self.add((employee_name,))
-            self.update_unique_attrs((employee_name,))
-            res_id = self.select_where(('employee_id',), f"employee_name = '{employee_name}'")
-        return res_id[0]
+    def select_all(self):
+        data = self.adapter.select(self.table_name)
+        res_data = []
+        for rec in data:
+            laid_by = self.related_table.id_to_name(rec[6])
+            rolled_by = self.related_table.id_to_name(rec[7])
+            res_rec = rec[:6] + laid_by + rolled_by + rec[8:]
+            res_data.append(res_rec)
+        return res_data
